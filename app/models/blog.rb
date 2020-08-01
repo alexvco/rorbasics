8167116388c3173bbbf1038ae4c3464f52bd57c5
alexv7 <alexvarjabedian7@gmail.com>
2020-07-31 22:24:03 -0700

    association of models has_many, belongs_to, polymorphic, etc..

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,6 @@
 class Blog < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, foreign_key: :user_id, class_name: 'Author'
+  has_many :images, as: :imageable, class_name: 'Image', dependent: :destroy
+
+  enum status: { Private: 0, Paid: 1, Public: 2 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :blogs
+  has_many :images, as: :imageable, class_name: 'Image', dependent: :destroy
 end
