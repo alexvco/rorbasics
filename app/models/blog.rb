d9af2be477ea7ed5ad899704253be7f2e27433e4
alexv7 <alexvarjabedian7@gmail.com>
2020-08-04 11:21:52 -0700

    delegate method

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,12 @@ class Blog < ApplicationRecord
   has_and_belongs_to_many :categories, join_table: :blogs_categories
 
   enum status: { Private: 0, Paid: 1, Public: 2 }
+
+  # delegate allows you to do this: Blog.first.author_first_name or without the prefix option: Blog.first.first_name
+  # instead of Blog.first.author.first_name, do not be fooled it does not solve any n + 1
+  # (it makes 2 db queries, 1 for the blog and 1 for the author as opposed to a join)
+  # it is identical to: def author_first_name; author.first_name; end
+  # the allow_nil: true option is to return Blog.first.author_first_name nil as opposed to an error when author is nil
+  # more on delegate here: https://apidock.com/rails/Module/delegate
+  delegate :first_name, to: :author, prefix: :author, allow_nil: true
 end
