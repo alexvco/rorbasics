4291ae38e331ab314beb0480be6b9fa996a086f5
alexv7 <alexvarjabedian7@gmail.com>
2020-08-08 13:31:46 -0700

    sql for querying HABTM attributes and syntax for combining/merging 2 ActiveRecord::Relation Object

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,6 +12,36 @@ class BlogsController < ApplicationController
       @blogs = Blog.where('user_id = ? OR status = ? OR status = ?', current_user.id, 1, 2)
       # @blogs = current_user.blogs.or( Blog.where(status: 1).or(Blog.where(status: 2)) ) # I prefer the SQL syntax much better
     end
+
+    return @blogs if params[:search].blank?
+
+    sql = <<-SQL
+      (select b.id from blogs b
+      left join blogs_categories bc on bc.blog_id = b.id
+      left join categories c on c.id = bc.category_id
+      where b.title like ?
+      or c.name = ?)
+    SQL
+
+    search_params = params[:search]
+    blog_ids = Blog.find_by_sql([sql, "\%#{search_params}\%", "#{search_params}"]).pluck(:id)
+
+    blogs_from_search = Blog.where(id: blog_ids)
+    @blogs = @blogs.merge(blogs_from_search)
+
+    # Rails: Combine Two ActiveRecord::Relation Objects
+    # combine using AND (intersection), use merge
+    # combine using OR (union), use or
+
+    # Examples
+    # Blog.where('id < 1').merge(Blog.where(id: 4))
+    # => #<ActiveRecord::Relation []>
+    # Blog.where('id < 1').or(Blog.where(id: 4))
+    # => #<ActiveRecord::Relation [#<Blog id: 4, user_id: 3, title: "Joes paid blog 1", body: "content for joes paid blog 1", status: "Paid", created_at: "2020-08-02 07:26:37", updated_at: "2020-08-02 07:26:37">]>
+    # Blog.where('id in (2,4)').merge(Blog.where(id: 4))
+    # => #<ActiveRecord::Relation [#<Blog id: 4, user_id: 3, title: "Joes paid blog 1", body: "content for joes paid blog 1", status: "Paid", created_at: "2020-08-02 07:26:37", updated_at: "2020-08-02 07:26:37">]>
+
+
     # Syntax to render default 404 page
     # render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
   end
